234e410bcb9aade0ce228921809ebdc89e625381
Jeffrey Cho <jeffcho59@gmail.com>
2014-10-28 17:54:58 -0400

    keys

---
+++ twitteroauth/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'XVKkg0jgAiPAVZCkH0GoeLltF');
+define('CONSUMER_SECRET', 'adzCZLxcS7J1HMpSjbKLx9clHBizHLUDhYMIPQydCVwCFWzMIm');
+define('OAUTH_CALLBACK', 'http://web.njit.edu/~jc389/twitter/twitteroauth/callback.php');
