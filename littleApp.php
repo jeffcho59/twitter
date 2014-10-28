e6e550aaa9f735d909a4e4282d3d222949e81c45
Jeffrey Cho <jeffcho59@gmail.com>
2014-10-28 13:33:17 -0400

    error

--- littleApp.php
+++ littleApp.php
@@ -11,14 +11,14 @@ $settings = array(
 );
 
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
- 
 $requestMethod = "GET";
- 
 $getfield = '?screen_name=iagdotme&count=20';
- 
 $twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetfield($getfield)
-             ->buildOauth($url, $requestMethod)
-             ->performRequest();
-			 
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+echo "<pre>";
+print_r($string);
+echo "</pre>";			 
 ?>
