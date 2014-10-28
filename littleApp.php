c1aebe6f55625c7464f1bf45689968d50cbff364
Jeffrey Cho <jeffcho59@gmail.com>
2014-10-28 13:34:50 -0400

    tweets

--- littleApp.php
+++ littleApp.php
@@ -12,13 +12,22 @@ $settings = array(
 
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 $requestMethod = "GET";
-$getfield = '?screen_name=iagdotme&count=20';
+if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "chojeffrey";}
+if (isset($_GET['count'])) {$user = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=$user&count=$count";
 $twitter = new TwitterAPIExchange($settings);
 $string = json_decode($twitter->setGetfield($getfield)
 ->buildOauth($url, $requestMethod)
 ->performRequest(),$assoc = TRUE);
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-echo "<pre>";
-print_r($string);
-echo "</pre>";			 
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }			 
 ?>
